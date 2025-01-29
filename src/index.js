d8d5d181aa585798918e0c41d9b12454624d9ed1
gihongeorge <gihongeorge2001@gmail.com>
2025-01-29 02:04:55 -0500

    updated index.js to create AI poem

---
+++ src/index.js
@@ -0,0 +1,25 @@
+   function displayPoem(response) {
+    new Typewriter("#poem", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+   }
+   function generatePoem(event) {
+    event.preventDefault();
+  
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apikey="cb1a55e07974f905394f1f28oeta9363"
+    let context= "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions. ";
+    let prompt=`User instructions: Generate a French Poem about ${instructionsInput.value}`;
+    let apiurl="https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}"
+
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiURL).then(displayPoem); 
+  }
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
